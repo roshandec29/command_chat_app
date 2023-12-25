396517ac454a1c8eb8096ffd64de4616b9992eb6
roshandec29 <roshandec252@gmail.com>
2023-12-25 13:00:20 +0530

    Delete chat_server directory

--- chat_server/chat_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ChatMessage
-
-class ChatMessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ChatMessage
-        fields = ['command', 'text', 'sender']
--- chat_server/chat_app/urls.py
+++
@@ -1,7 +0,0 @@
-# chat_app/urls.py
-from django.urls import path
-from .views import ProcessChat
-
-urlpatterns = [
-    path('chat/', ProcessChat.as_view(), name='process_chat'),
-]
